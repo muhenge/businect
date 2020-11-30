bfbddb0e8b0d60ef42e465adde777db6f4401eb7
muhenge <mugunga5@gmail.com>
2020-12-01 00:47:36 +0200

    Add user comments validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  has_many :comments
+  has_many :active_relationships, class_name: "Relationships"
+  foreign_key: "follower_id",
+  dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_181507) do
+ActiveRecord::Schema.define(version: 2020_11_30_221456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -78,5 +86,6 @@ ActiveRecord::Schema.define(version: 2020_11_30_181507) do
     t.index ["voter_type", "voter_id"], name: "index_votes_on_voter_type_and_voter_id"
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
