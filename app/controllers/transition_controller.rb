0d6f1be0c84af90dcda0d2ab0bd81b6cba5a6844
muhenge <mugunga5@gmail.com>
2020-11-30 18:22:37 +0200

    Add transitional page after sign up

---
+++ app/controllers/transition_controller.rb
@@ -0,0 +1,8 @@
+class TransitionController < ApplicationController
+  include UsersHelper
+  # before_action :set_user, only: [:show]
+
+  def show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'transition/show'
   devise_for :companies
   get 'public/index'
   get 'public/private'
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations"}
   root to: "public#index"
   resources :posts
   resources :users
