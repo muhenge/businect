2efeb23b1dca36dc4c2eb1d239c86d658b982835
muhenge <mugunga5@gmail.com>
2020-12-02 17:19:05 +0200

    Add materialize css in webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import '../css/application'
+import '../css/style'
+
+import 'materialize-css/dist/js/materialize'
+
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
