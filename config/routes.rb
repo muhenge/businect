a0e4c6a1f274f517ffb3f736febb7992ec07ef41
muhenge <mugunga5@gmail.com>
2020-12-02 16:52:33 +0200

    Add redirect for devise sign up

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   devise_for :companies
   get 'public/index'
   get 'public/private'
-  devise_for :users, controllers: { registrations: "registrations"}
+  devise_for :users, controllers: { registrations: "registrations"} do
+    member do
+      get :following, :followers
+    end
+  end
   root to: "public#index"
   resources :posts
-  resources :users
+  # resources :users
 end
