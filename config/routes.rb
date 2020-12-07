71c21898a7315d2a175a2b3f5d84700cb7809429
muhenge <mugunga5@gmail.com>
2020-12-07 15:18:07 +0200

    Update user edit style

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,30 @@
 Rails.application.routes.draw do
+
   get 'transition/show'
   devise_for :companies
   get 'public/index'
   get 'public/private'
+
   devise_for :users, controllers: { registrations: "registrations"} do
     member do
       get :following, :followers
     end
   end
+ 
   root to: "public#index"
+
   resources :posts do
     member do
       put "like" => "posts#vote"
     end
-    
+    resources :users, only: [:show]
   end
+
   resources :comments
+
   resources :users do
     resources :posts, only: [:index]
   end
+
   get 'users/:id/user_posts' => 'users#user_posts', :as => :users_posts
 end
