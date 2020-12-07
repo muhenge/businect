63391f13923949d2e9a7b9cd4fa37dac46e95312
muhenge <mugunga5@gmail.com>
2020-12-07 16:51:13 +0200

    Add model to posts not yet done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,10 @@ import '../css/style'
 
 import 'materialize-css/dist/js/materialize'
 
-
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+ 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
