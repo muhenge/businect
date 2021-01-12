cf68f02c4d6a71d8238c831745b1340595fa40c8
Herve <mugunga5@gmail.com>
2021-01-12 23:00:48 +0200

    Edit sign up page style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_devise_parameters, if: :devise_controller?
     protected
     def configure_devise_parameters
-        devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username,:email,:password,:interest_id)}
+        devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username,:email,:password,:password_confirmation,:interest_id)}
         devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:avatar, :username,:email,:bio,:about, :interest_id ,:password,:current_password)}
     end
 end
