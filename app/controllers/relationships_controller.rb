c60eab0e298e0680a9663af1c6e1f516e1603510
muhenge <mugunga5@gmail.com>
2021-01-14 23:12:01 +0200

    Correct followe/following pages

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,6 +5,7 @@ class RelationshipsController < ApplicationController
     def create
         @user = User.find(params[:followed_id])
         current_user.follow(@user)
+        
         respond_to do |format|
             format.html { redirect_to @user }
             format.js
@@ -13,6 +14,7 @@ class RelationshipsController < ApplicationController
     def destroy
         @user = Relationship.find(params[:id]).followed
         current_user.unfollow(@user)
+        redirect_to user_path(@user)
         respond_to do |format|
             format.html { redirect_to @user }
             format.js
